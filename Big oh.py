b1a5582b5e6090d2eca06265c8ce138ce3e2e624
K-SuhasM <k.suhasmandle@gmail.com>
2026-08-25 23:10:35 +0530

    Day 1. Learned the basics of time complexity.

---
+++ Big oh.py
@@ -0,0 +1,17 @@
+# def factt(x):
+#     answer = 1
+#     while x>1:
+#         answer *=x
+#         x -= 1
+#     return answer
+
+
+# print(factt(5))
+
+
+x = 5
+answer = 1
+while x>1:
+    answer = answer * x
+    x = x-1
+print(answer) 
---
+++ Time.py
@@ -0,0 +1,8 @@
+import time
+
+a = time.time()
+
+for i in range(1,101):
+    print(i)
+
+print(time.time()-a)
