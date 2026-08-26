f4575c8feb041e6d6863ca557bd482a0ab6ba8f6
K-SuhasM <k.suhasmandle@gmail.com>
2026-08-26 23:13:40 +0530

    Solved first leetcode problem

---
+++ 412 FizzBuzz.py
@@ -0,0 +1,58 @@
+'''
+
+
+
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+'''
+
+class solution: 
+    def fizzBuzz(self,n):
+        self.n = int(n)
+        answer = []  
+    # n = int(input())
+    # answer = []
+        for i in range(1,n+1):
+
+            if i%3 == 0 and i%5 == 0:
+                answer.append("FizzBuzz")
+    
+            elif i%3 == 0:
+                answer.append("Fizz")
+
+            elif i%5 == 0:
+                answer.append("Buzz")
+
+            else: answer.append(str(i))
+        print(answer)
+
+n1 = solution.fizzBuzz(15)
+
+
+
+# n = int(input())
+# answer = []
+
+# for i in range(1,n+1):
+
+#     if i%3 == 0 and i%5 == 0:
+#         answer.append("FizzBuzz")
+    
+#     elif i%3 == 0:
+#         answer.append("Fizz")
+
+#     elif i%5 == 0:
+#         answer.append("Buzz")
+
+#     else: answer.append(str(i))
+
+# print(answer)        
+    
+   
